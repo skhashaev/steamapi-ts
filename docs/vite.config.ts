cae54d6f2f5cffa5c05b122efa78c2d6814b0ecb
Shamil Khashaev <shamil.khashaev@gmail.com>
2025-11-24 00:42:31 +0300

    fix: add base url to github pages

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: 'https://skhashaev.github.io/steamapi-ts/',
   plugins: [react()],
   resolve: {
     alias: {
